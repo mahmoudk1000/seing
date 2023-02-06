e931f19374e8067adfbe331613b61ac9abcaa86d
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-02-06 17:59:30 +0200

    SEING: homePage (WIP)

--- seing.py
+++ seing.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     '''Homepage routeing function'''
-    return render_template("index.html")
+    return render_template("homePage.html")
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
