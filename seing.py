6dda2dcc3a71c173afe63fbad2b5370208db56ec
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-03-10 20:02:01 +0200

    seing: new routers

--- seing.py
+++ seing.py
@@ -1,14 +1,36 @@
 #! /usr/bin/env python
 
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def home():
     '''Homepage routeing function'''
-    return render_template("homePage.html")
+    if request.method == "POST":
+        q = request.form["q"]
+        return redirect(url_for("results", query=q))
+    else:
+        return render_template("homePage.html")
+
+
+@app.route("/results?<query>")
+def results(query):
+    if request.method == "GET":
+        return render_template("results.html", q=query)
+    else:
+        return redirect(url_for("/"))
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
