1a9ea8ed792f9e3fdfcf176db55cb47a9404412e
Mahmoud Ayman <mahmoudk1000@gmail.com>
2023-05-01 22:09:29 +0300

    hmm

--- shell.nix
+++ shell.nix
@@ -1,10 +1,32 @@
 { pkgs ? import <nixpkgs> {} }:
 
+# let
+#   googlesearch = pkgs.python3Packages.mkShell {
+#      name = "googlesearch";
+#      buildInputs = [
+#        pkgs.python3Packages.pip
+#        (pkgs.python3Packages.buildPythonPackage {
+#          pname = "googlesearch";
+#          version = "0.1";
+#          src = pkgs.fetchgit {
+#            url = "https://github.com/Nv7-GitHub/googlesearch.git";
+#            rev = "master";
+#          };
+#          propagatedBuildInputs = with pkgs.python3Packages; [
+#            requests
+#            beautifulsoup4
+#          ];
+#        })
+#      ];
+#    };
+# in
+
 pkgs.mkShell rec {
   name = "seing";
   venvDir = "./.venv";
 
   packages = [
+    # googlesearch
     (pkgs.python3.withPackages (ps: [
       ps.flask
       ps.venvShellHook
@@ -21,6 +43,7 @@ pkgs.mkShell rec {
       ps.fuzzywuzzy
       ps.geoip2
       ps.numpy
+      ps.python-Levenshtein
     ]))
   ];
 
